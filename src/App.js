08bf974515ac78e992bbb5fbe254dae2ebaa5d52
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-08 13:03:46 +0430

    add navbar section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      <h1>salam Arash Hosseini</h1>
+      <HomeScreen />
     </div>
   );
 }
