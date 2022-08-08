b57224efe1b160d822c15cd2f8ea117cdd823324
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-09 03:50:38 +0430

    deploy on firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJptU4Yn4UYCgNEFki4aakJyMngpfU6MA",
+    authDomain: "netflix-clone-react-3d845.firebaseapp.com",
+    projectId: "netflix-clone-react-3d845",
+    storageBucket: "netflix-clone-react-3d845.appspot.com",
+    messagingSenderId: "927129616843",
+    appId: "1:927129616843:web:d936509ea2f1b6b0c2b64e"
+  };
