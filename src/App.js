c4ea65a3e1a7fb6c3e861a0480dee06bf186c82a
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-14 17:59:11 +0430

    building the login screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      {!user ? (
+        <LoginScreen />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/test" element={<h1>salam</h1>} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
