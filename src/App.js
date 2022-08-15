839210afa817c89102b80db6278c960fdaabe45e
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-16 00:41:40 +0430

    implementing redux

--- src/App.js
+++ src/App.js
@@ -9,17 +9,25 @@ import {
 } from "react-router-dom";
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import {login,logout, selectUser} from './features/userSlice'
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
       if (userAuth) {
         //Logged in
         console.log(userAuth)
+        dispatch(login({
+          uid:userAuth.uid,
+          email:userAuth.email
+        }))
       } else {
         //Logged out
+        dispatch(logout)
       }
 
     })
