c79bc95ed34631bb3bfa688f595124306a58ed2a
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-15 23:57:21 +0430

    setting up authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen'
@@ -7,9 +7,25 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        //Logged in
+        console.log(userAuth)
+      } else {
+        //Logged out
+      }
+
+    })
+
+    return unsubscribe;
+  }, [])
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth } from "firebase/auth";
+
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDJptU4Yn4UYCgNEFki4aakJyMngpfU6MA",
-    authDomain: "netflix-clone-react-3d845.firebaseapp.com",
-    projectId: "netflix-clone-react-3d845",
-    storageBucket: "netflix-clone-react-3d845.appspot.com",
-    messagingSenderId: "927129616843",
-    appId: "1:927129616843:web:d936509ea2f1b6b0c2b64e"
-  };
+  apiKey: "AIzaSyDJptU4Yn4UYCgNEFki4aakJyMngpfU6MA",
+  authDomain: "netflix-clone-react-3d845.firebaseapp.com",
+  projectId: "netflix-clone-react-3d845",
+  storageBucket: "netflix-clone-react-3d845.appspot.com",
+  messagingSenderId: "927129616843",
+  appId: "1:927129616843:web:d936509ea2f1b6b0c2b64e"
+};
+
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
+const auth = getAuth(firebaseApp)
+// firebase.auth().createUserWithEmailAndPassword
+
+
+export { auth }
+export default db;
